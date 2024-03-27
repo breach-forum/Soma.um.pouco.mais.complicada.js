de1dfc443cc25ca6736e2856c86b116a33745c23
breach-forum <157008483+breach-forum@users.noreply.github.com>
2024-03-27 12:18:04 -0300

    Soma.js

---
+++ Soma.js
@@ -0,0 +1,9 @@
+// Solicita dois números inteiros ao usuário
+var A = parseInt(prompt("Digite o primeiro número inteiro:"));
+var B = parseInt(prompt("Digite o segundo número inteiro:"));
+
+// Efetua a soma dos números e atribui o resultado à variável X
+var X = A + B;
+
+// Imprime o resultado em forma decimal
+console.log("X = " + X.toFixed(2));
